5ff8f7ab9d2cac3775392d7b5cbd6f027db823a0
tyoma <tvtyyiovsv@gmail.com>
2025-02-07 22:17:19 +0100

    initial

---
+++ components/index.ts
@@ -0,0 +1,12 @@
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+import CustomButton from "./CustomButton";
+
+export { 
+    Hero, 
+    CustomButton,
+    Navbar,
+    Footer,
+};
